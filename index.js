0abba43df0c354bceb0d44072814775b864c97c8
mandusaheb <manavchahare@gmail.com>
2025-11-24 02:45:58 +0530

    Modify welcome message and formatting
    Updated welcome message title and added spacing.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("guildMemberAdd", async (member) => {
     if (!channel) return;
 
     const welcomeEmbed = new EmbedBuilder()
-        .setTitle("🎉 Welcome to the cosmos!")
+        .setTitle(" 💫cosmos gate!")
         .setDescription(`A silent watcher of the stars greets all who step into the void.!`)
         .setColor("#00BFFF")
         .setImage(`https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzBya3NuOG5pZGp1aXh4bTR0a2htcnNxbWpreXIzZjh2djd6enV0ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/37IzUsLdfChayL5uyA/giphy.gif`)
@@ -54,6 +54,7 @@ client.on("messageCreate", (msg) => {
 // Login
 
 client.login(process.env.TOKEN);
+
 
 
 
